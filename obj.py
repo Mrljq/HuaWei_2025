e635775a204394680935691d09dbc08dfd7a8baa
Mrljq <1057101380@qq.com>
2025-03-12 17:16:15 +0800

    修改

--- obj.py
+++ obj.py
@@ -6,31 +6,34 @@ import numpy as np
 REP_NUM = 3
 
 
-class Object:
-    def __init__(self):
-        self.replica = [0 for _ in range(REP_NUM + 1)]
-        self.unit = [[] for _ in range(REP_NUM + 1)]
-        self.size = 0
-        self.lastRequestPoint = 0
-        self.isDelete = False
+
+
+# class Object:
+#     def __init__(self):
+#         self.replica = [0 for _ in range(REP_NUM + 1)]
+#         self.unit = [[] for _ in range(REP_NUM + 1)]
+#         self.size = 0
+#         self.lastRequestPoint = 0
+#         self.isDelete = False
 
 class Obj_State:
     def __init__(self):
         self.state_table = {}
-        #state,position1,position2,position3
+        #state,tag,size
         #state:0删除，1存在 postion(list):start_position,end_position
     def del_obj(self,obj_id):
         if obj_id in self.state_table.keys():
             self.state_table[obj_id][0] = 0
-    def insert_obj(self,obj_id,p1,p2,p3):
+    def insert_obj(self,obj_id,tag,size):
         self.state_table[obj_id] = []
         self.state_table[obj_id].append(1)
-        self.state_table[obj_id].append(p1)
-        self.state_table[obj_id].append(p2)
-        self.state_table[obj_id].append(p3)
+        self.state_table[obj_id].append(tag)
+        self.state_table[obj_id].append(size)
+
+obj_state = Obj_State()
 
 class disk_state:
-    def __init__(self, storge_space):
+    def __init__(self, storge_space, m):
         self.storge_space = np.full(storge_space, -1)
         self.point_index = 0
         self.read_s = 64
@@ -38,6 +41,9 @@ class disk_state:
         self.point_
         self.left_G
         self.already_storge = 0#已经存储的大小
+        self.discrete_space = {} #离散空间
+        for i in m:
+            self.discrete_space[i] = []
     
     def insert(self, obj_id, size, index):#插入时将占用的空间用对象id修改，-1代表没有占用
         self.storge_space[index:index+size] = obj_id
@@ -60,6 +66,7 @@ class disk_state:
         self.point_sequence += tem[:self.point_index]
     #==================该函数用于获取指针序列======================
     #np.array([5,5, 1, 1, 1, 2, 2, 0, 0, 0])
+    #========(储存单元下标，对象id，size)============================
     #[(0, 5, 2), (2, 1, 2), (4, 2, 2), (6, 0, 3)]
     def find_sequences(self):
         # 找到值变化的位置
@@ -72,29 +79,43 @@ class disk_state:
     
     def del_obj(self, obj_id):
         # 计算数组中1的个数
-        size = np.sum(self.storge_space == obj_id)
+        size = obj_state.state_table[obj_id][2]
+        tag = obj_state.state_table[obj_id][1]
         self.already_storge -= size
         # 将数组中的所有1替换为-1
         self.storge_space[self.storge_space == obj_id] = -1
+        # 使用numpy.where找到特定元素的所有下标
+        indices = np.where(self.storge_space == obj_id)[0]
+        self.discrete_space[tag].append((indices,size))
 
     def judge(self, size):
         if (self.already_storge + size) >= 0.9*len(self.storge_space):
             return False
         else:
             return True
+    
 
 
+#===============该类用于在插入时告诉插入位置=========================
 class Div_Disk_Space:
     def __init__(self, storge_space, n ,free_data_array,m):
         self.dif_space_point_index = {}#用来存储所有硬盘划分后每一个类的指针位置，从指针位置开始插入
         self.percentage = []
         self.compute_percentage(free_data_array,m,storge_space)
         for i in n:
-            self.dif_space_point_index[i] = []
+            self.dif_space_point_index[i] = []#每一个硬盘self.dif_space_point_index[i]有一个list[(ori1,current1),(ori2,current2)]
             p = 0
             for i1 in self.percentage:
-                self.dif_space_point_index = p
+                self.dif_space_point_index[i].append(p,p)
                 p += i1
+        self.discrete_space = {}
+        for i in n:
+            self.discrete_space[i] = {}
+            for i1 in m:
+                self.discrete_space[i][i1] = []
+
+        
+        
 
     def compute_percentage(self,free_data_array,m,storge_space):
         w_d = free_data_array[m:2*m]-free_data_array[0:m]
@@ -111,7 +132,7 @@ class Div_Disk_Space:
         self.percentage = storge_space *cul_write / total_sum
 
     def insert(self, obj_class, size, disk_id):        
-        self.dif_space_point_index[disk_id][obj_class] += size
+        self.dif_space_point_index[disk_id][obj_class][1] += size
 
 
-        
+#====================用一个循环首先judge每一个盘够不够0.9space，然后使用离散空间，然后看能不能顺存===============
